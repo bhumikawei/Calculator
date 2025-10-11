78874b945fe28b18d57eb0511030927bd07aeb0d
Bhumika <bhumika04shimla@gmail.com>
2025-10-11 15:48:25 +0530

    made full calculator

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -6,12 +6,24 @@ import Display from './Components/Display'
 
 function App() {
   const [calVal, setCalVal] = useState("");
+
+  const handleButtons = (buttonText) => {
+    if(buttonText === 'C'){
+      setCalVal("");
+    } else if(buttonText === '='){
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplay = calVal + buttonText;
+      setCalVal(newDisplay);
+    }
+  }
   return (
     <center>
     <h1>Calculator</h1>
     <div className='container'>
       <Display display={calVal}/>
-      <ButtonContainer/>
+      <ButtonContainer button={handleButtons}/>
     </div>
     </center>
   )
--- Calculator/src/Components/ButtonContainer.jsx
+++ Calculator/src/Components/ButtonContainer.jsx
@@ -1,9 +1,9 @@
-const ButtonContainer = () => {
+const ButtonContainer = ({button}) => {
 
   const buttonNames = ['1', '2', '3', 'C', '4' ,'5', '6', '+', '7', '8', '9', '-', '=', '0', '*', '/']
   return (
     <div className="ButtonContainer">
-    {buttonNames.map((buttonName => <button key={buttonName}>{buttonName}</button>))}
+    {buttonNames.map((buttonName => <button key={buttonName} onClick={() => button(buttonName)}>{buttonName}</button>))}
     </div>
   )
 }
