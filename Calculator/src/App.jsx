ba033b61ce2393f3c7faf5ff3cca23abe4ec4423
Bhumika <bhumika04shimla@gmail.com>
2025-10-11 15:37:58 +0530

    added styling ig

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -5,13 +5,13 @@ import Display from './Components/Display'
 
 function App() {
   return (
-    <>
+    <center>
     <h1>Calculator</h1>
     <div className='container'>
       <Display/>
       <ButtonContainer/>
     </div>
-    </>
+    </center>
   )
 }
 
--- Calculator/src/Components/ButtonContainer.jsx
+++ Calculator/src/Components/ButtonContainer.jsx
@@ -2,10 +2,9 @@ const ButtonContainer = () => {
 
   const buttonNames = ['1', '2', '3', 'C', '4' ,'5', '6', '+', '7', '8', '9', '-', '=', '0', '*', '/']
   return (
-    <>
+    <div className="ButtonContainer">
     {buttonNames.map((buttonName => <button key={buttonName}>{buttonName}</button>))}
-    
-    </>
+    </div>
   )
 }
 
